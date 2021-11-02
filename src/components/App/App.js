52c915e07411fc16c4e9bc6b205a99361ded0e07
nazifbara <nazifbarassounon@gmail.com>
2021-11-02 13:14:12 +0100

    show cart on the app bar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,21 +1,42 @@
 import { Switch, Route } from 'react-router-dom';
-import { Typography, AppBar, Box, Toolbar, Container } from '@mui/material';
+import {
+  Typography,
+  AppBar,
+  Box,
+  Toolbar,
+  Container,
+  IconButton,
+  Badge,
+} from '@mui/material';
+import { ShoppingCart } from '@mui/icons-material';
 
 import routes from '../../routes';
 import { Link } from '../../components';
+import { useCart } from '../../hooks/useCart';
 
 function App() {
+  const { cartProducts } = useCart();
   return (
     <>
       <Box>
         <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" noWrap component="div">
-              <Link to="/" color="inherit" underline="none">
-                Electronics Shop
-              </Link>
-            </Typography>
-          </Toolbar>
+          <Container maxWidth="lg">
+            <Toolbar>
+              <Typography variant="h6" noWrap component="div">
+                <Link to="/" color="inherit" underline="none">
+                  Electronics Shop
+                </Link>
+              </Typography>
+              <Box sx={{ flexGrow: 1 }} />
+              <Box>
+                <IconButton color="inherit" size="large">
+                  <Badge badgeContent={cartProducts.length} color="error">
+                    <ShoppingCart />
+                  </Badge>
+                </IconButton>
+              </Box>
+            </Toolbar>
+          </Container>
         </AppBar>
       </Box>
       <Container maxWidth="md">
