8de2970417a3c7ed0617ff66fd8566295667cccc
nazifbara <nazifbarassounon@gmail.com>
2021-11-05 17:55:08 +0100

    add sign in and sigin up tabs

--- src/components/LoginDialog/LoginDialog.js
+++ src/components/LoginDialog/LoginDialog.js
@@ -1,8 +1,12 @@
-import { Dialog, DialogTitle, IconButton } from '@mui/material';
+import { useState } from 'react';
+import { Dialog, DialogTitle, IconButton, Tabs, Tab, Box } from '@mui/material';
 import { Close } from '@mui/icons-material';
 
 const LoginDialog = (props) => {
   const { onClose, open } = props;
+  const [activeTab, setActiveTab] = useState(0);
+
+  const handleTabChange = (e, value) => setActiveTab(value);
 
   return (
     <Dialog onClose={onClose} open={open}>
@@ -12,6 +16,14 @@ const LoginDialog = (props) => {
         </IconButton>
         Login
       </DialogTitle>
+      <Box sx={{ maxWidth: 500, width: '90vw', height: '90vh' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={activeTab} onChange={handleTabChange} centered>
+            <Tab label="Sign In" />
+            <Tab label="Sign Up" />
+          </Tabs>
+        </Box>
+      </Box>
     </Dialog>
   );
 };
